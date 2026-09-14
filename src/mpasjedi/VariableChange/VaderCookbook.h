a117991359c0890d7e3b6dfba4d119f420c31186
Huiying.Luo <huiying.luo@noaa.gov>
2026-09-14 15:36:14 -0400

    add recipes

--- src/mpasjedi/VariableChange/VaderCookbook.h
+++ src/mpasjedi/VariableChange/VaderCookbook.h
@@ -19,7 +19,11 @@ namespace mpas {
 
   static std::map<std::string, std::vector<std::string>> vaderMPASCustomCookbook() {
     return {
-      // tv: from t and q
+      // for temp recipe testing
+      {"dry_air_density",                   {"DryAirDensity_A"}},
+      // pm25 from fine smoke and dust in RRFSv2
+      {"mass_density_of_particulate_matter_2p5_in_air", {"ParticulateMatter2p5_C"}}
+      // tv: from t and q 
       {"virtual_temperature",          {"AirVirtualTemperature_A"}}
     };
   }
